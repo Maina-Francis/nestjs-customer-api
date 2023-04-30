d5cbdf733ec967a9bd753198aa09c9e2dec44487
Francis Maina <sebastianfrancix@gmail.com>
2023-05-01 00:57:00 +0300

    added HttpException incase we can't find user by id

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CustomersService } from 'src/customers/services/customers/customers.service';
 
 @Controller('customers')
 export class CustomersController {
+  // dependency injection
   constructor(private customersService: CustomersService) {}
 
   // Get controller
-  @Get()
-  getcustomer() {
-    return this.customersService.findCustomer();
+  @Get(':id')
+  getcustomer(@Param('id', ParseIntPipe) id: number) {
+    // console.log(typeof id);
+
+    const customer = this.customersService.findCustomerById(id);
+
+    if (customer) {
+      return customer;
+    }
+    throw new HttpException('Customer not found', HttpStatus.NOT_FOUND);
   }
 }
--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,11 +2,26 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-  findCustomer() {
-    return {
+  // Sample users array
+  private users = [
+    {
       id: 1,
-      email: 'test@gmail.com',
+      email: 'test@example.com',
       createdAt: new Date(),
-    };
+    },
+    {
+      id: 2,
+      email: 'user@example.com',
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      email: 'sample@example.com',
+      createdAt: new Date(),
+    },
+  ];
+
+  findCustomerById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }
