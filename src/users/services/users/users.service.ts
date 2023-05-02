9a92a7dcc3555bc4bd9216d3e953de4afa667198
Francis Maina <sebastianfrancix@gmail.com>
2023-05-02 11:58:24 +0300

    added users interface and get users service and controller

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/types/user';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    {
+      username: 'Francis',
+      password: '12345',
+    },
+    {
+      username: 'Maina',
+      password: 'maina',
+    },
+    {
+      username: 'Denis',
+      password: 'denis',
+    },
+  ];
+
+  getUser() {
+    return this.users;
+  }
+
+  getUserByUsername(username) {
+    return this.users.find((user) => user.username === username);
+  }
+}
